2aa9ad65f02b8068d851dafe99ec26b0a00c3622
Derflin <cubussledz1@gmail.com>
2021-12-03 16:26:31 +0100

    GUI Added

--- applicationGui/Program.cs
+++ applicationGui/Program.cs
@@ -18,9 +18,6 @@ namespace applicationGui
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
